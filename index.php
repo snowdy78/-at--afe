23bb01fecb05a78cb768b89029035c34287a2eae
themrleoner12@mail.ru <themrleoner12@mail.ru>
2024-05-12 14:22:20 +0700

    asdfsa

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bootstrap demo</title>
+		<link href="/css/custom.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="dropdown" data-bs-theme="light">
+			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonLight" data-bs-toggle="dropdown" aria-expanded="false">
+				Default dropdown
+			</button>
+			<ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonLight">
+				<li><a class="dropdown-item active" href="#">Action</a></li>
+				<li><a class="dropdown-item" href="#">Action</a></li>
+				<li><a class="dropdown-item" href="#">Another action</a></li>
+				<li><a class="dropdown-item" href="#">Something else here</a></li>
+				<li><hr class="dropdown-divider"></li>
+				<li><a class="dropdown-item" href="#">Separated link</a></li>
+			</ul>
+		</div>
+
+		<div class="dropdown" data-bs-theme="dark">
+			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonDark" data-bs-toggle="dropdown" aria-expanded="false">
+				Dark dropdown
+			</button>
+			<ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonDark">
+				<li><a class="dropdown-item active" href="#">Action</a></li>
+				<li><a class="dropdown-item" href="#">Action</a></li>
+				<li><a class="dropdown-item" href="#">Another action</a></li>
+				<li><a class="dropdown-item" href="#">Something else here</a></li>
+				<li><hr class="dropdown-divider"></li>
+				<li><a class="dropdown-item" href="#">Separated link</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
